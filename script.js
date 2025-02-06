ed0a61dd4d6e5205986f762d3a43b4f86c36b84e
KDMEHTA704 <131295594+KDMEHTA704@users.noreply.github.com>
2025-02-06 13:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-  const charCount = {};
-  for (let i of str) {
-    charCount[i] = (charCount[i] || 0) + 1;
-  }
-    if (charCount[i] === 1) {
-      return i;
+    let charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];  
+        if (charCount[char] === undefined) { 
+            charCount[char] = 1;
+        } else {
+            charCount[char]++;
+        }
     }
-  }
-  return null;
-}
+
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+
+    return null;}
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
